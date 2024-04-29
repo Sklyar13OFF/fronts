dea37c300a7827d7f2bbd11a7a64706b09e0c5f4
Daniel Sklyarov <dsklyarov8@gmail.com>
2024-04-29 21:52:47 +0300

    comm

--- api/ApiWrapper.jsx
+++ api/ApiWrapper.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000/v1/'
+const BASE_URL = 'https://127.0.0.1:8000/v1/'
 const BASE_FRONT = 'http://localhost:3000/'
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
